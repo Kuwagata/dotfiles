83c002b8de7f65cd679135566a7c352fee226986
John Heatherington <jheatherington@gmail.com>
2023-12-07 13:23:07 -0500

    Neovim: Harpoon now uses standard setup
    See:
    https://github.com/ThePrimeagen/harpoon/commit/70703d6ce1da56a07c50ad2dadc894410b9ebe34

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -4,9 +4,6 @@ return {
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
-		config = function(_, opts)
-			require("harpoon"):setup(opts)
-		end,
 		keys = {
 			{
 				"<leader>a",
