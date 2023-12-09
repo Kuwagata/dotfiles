2be6d0dddd8dd81dfbea711c02f92f2520e1bfcc
John Heatherington <jheatherington@gmail.com>
2023-12-08 22:14:20 -0500

    Neovim: Added nvim-dap-ui

--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -101,4 +101,36 @@ return {
 			},
 		},
 	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		lazy = true,
+		opts = {},
+		config = function(_, opts)
+			local dap, dapui = require("dap"), require("dapui")
+
+			dapui.setup(opts)
+
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+		keys = {
+			{
+				"<leader>du",
+				function()
+					require("dapui").toggle()
+				end,
+				desc = "Toggle UI",
+			},
+		},
+	},
 }
