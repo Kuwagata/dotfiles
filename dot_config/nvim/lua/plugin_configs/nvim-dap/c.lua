86d6e7343d587d08e28156d41d43990ad24fe700
John Heatherington <jheatherington@gmail.com>
2023-12-08 23:06:15 -0500

    Neovim: Cleaning up nvim-dap

--- dot_config/nvim/lua/plugin_configs/nvim-dap/c.lua
+++ dot_config/nvim/lua/plugin_configs/nvim-dap/c.lua
@@ -1,47 +1,42 @@
 vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("nvimdap_c", {}),
 	pattern = "c",
 	callback = function(_)
 		local dap = require("dap")
 
-		if dap.adapters.gdb == nil then
-			dap.adapters.gdb = {
-				type = "executable",
-				command = "gdb",
-				args = { "-i", "dap" },
-			}
-		end
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "-i", "dap" },
+		}
 
-		if dap.adapters.lldb == nil then
-			dap.adapters.lldb = {
-				type = "executable",
-				command = "/usr/bin/lldb-vscode",
-				name = "lldb",
-			}
-		end
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "/usr/bin/lldb-vscode",
+			name = "lldb",
+		}
 
-		if dap.configurations.c == nil then
-			dap.configurations.c = {
-				{
-					name = "Launch GDB",
-					type = "gdb",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					cwd = "${workspaceFolder}",
-				},
-				{
-					name = "Launch LLDB",
-					type = "lldb",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-					args = {},
-				},
-			}
-		end
+		dap.configurations.c = {
+			{
+				name = "Launch GDB",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+			},
+			{
+				name = "Launch LLDB",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+		}
 	end,
 })
